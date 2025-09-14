ed44e9e69ffc589bdf7aed4d084337306e14084f
Eugenia Barrios <eugeb2024@gmail.com>
2025-09-14 11:51:14 -0300

    Se creo carpeta POO-SmartHome

---
+++ PROGRAMACIÓN I/POO-SmartHome/usuarios.py
@@ -0,0 +1,8 @@
+# usuarios.py
+class Usuario:
+    def __init__(self, id, nombre, correo, contrasena, rol):
+        self.id = id
+        self.nombre = nombre
+        self.correo = correo
+        self.contrasena = contrasena
+        self.rol = rol
