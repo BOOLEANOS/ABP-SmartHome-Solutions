415b28c6d07f0c818b304823be24a52da5bd1698
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-09-14 21:46:35 -0300

    Update dispositivos.py

--- PROGRAMACIÓN I/POO-SmartHome/dispositivos.py
+++ PROGRAMACIÓN I/POO-SmartHome/dispositivos.py
@@ -1,1 +1,17 @@
 
+class Dispositivo: 
+  def __init__(self, id, nombre, tipo, estado=True, usuario_id=None):
+    self.id = id
+    self.nombre = nombre
+    self.tipo = tipo
+    self.estado = estado
+    self.usuario_id = usuario_id
+
+  def mostrar_info(self):
+    estado_str = "Encendido" if self.estado else "Apagado"
+    print(f"ID: {self.id} | Nombre: {self.nombre} | Tipo: {self.tipo} | Estado: {estado_str}")
+
+class GestionDispositivos:
+  def __init__(self):
+    self.dispositivos = []
+    
