d7381b91c8107766f48ca8b9c4245ef88e3ea08e
Nanu <nancy.morales.387.06@gmail.com>
2025-09-15 18:30:45 -0300

    actualización

--- PROGRAMACIÓN I/POO-SmartHome/dispositivos.py
+++ PROGRAMACIÓN I/POO-SmartHome/dispositivos.py
@@ -6,3 +6,13 @@ class Dispositivo:
         self.estado_dispositivo = estado_dispositivo
         self.id_tipo = id_tipo
         self.id_usuario = id_usuario  
+    
+    def encender(self):
+        self.estado_dispositivo = True
+
+    def apagar(self):
+        self.estado_dispositivo = False
+
+    def __str__(self):
+        estado = "Encendido" if self.estado_dispositivo else "Apagado"
+        return f"Dispositivo(ID: {self.id_dispositivo}, Nombre: {self.nombre_dispositivo}, Ubicación: {self.ubicacion}, Estado: {estado})"
