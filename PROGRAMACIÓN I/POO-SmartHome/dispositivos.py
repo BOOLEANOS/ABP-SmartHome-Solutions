3cba5d0aa76733a866835deefd27b04b6ea36185
Nanu <nancy.morales.387.06@gmail.com>
2025-09-15 17:25:36 -0300

    refactor: mejora en la estructura de la clase Dispositivo

--- PROGRAMACIÓN I/POO-SmartHome/dispositivos.py
+++ PROGRAMACIÓN I/POO-SmartHome/dispositivos.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 class Dispositivo:
     def __init__(self, id_dispositivo, nombre_dispositivo, ubicacion, estado_dispositivo, id_tipo, id_usuario):
         self.id_dispositivo = id_dispositivo
@@ -8,13 +6,3 @@ class Dispositivo:
         self.estado_dispositivo = estado_dispositivo
         self.id_tipo = id_tipo
         self.id_usuario = id_usuario  
-=======
-class Dispositivo:
-    def __init__(self, id_dispositivo, nombre_dispositivo, ubicacion, estado_dispositivo, id_tipo, id_usuario):
-        self.id_dispositivo = id_dispositivo
-        self.nombre_dispositivo = nombre_dispositivo
-        self.ubicacion = ubicacion
-        self.estado_dispositivo = estado_dispositivo
-        self.id_tipo = id_tipo
-        self.id_usuario = id_usuario  
->>>>>>> c863cd6708ebfdc5c0ca17b8f21191ba18ee6b78
