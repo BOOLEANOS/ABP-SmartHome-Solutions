b029e2ae1b05f5d1dc348c534344caa95a9533be
Nanu <nancy.morales.387.06@gmail.com>
2025-09-15 19:47:07 -0300

    pytest

--- PROGRAMACIÓN I/POO-SmartHome/tests/test_dispositivos.py
+++ PROGRAMACIÓN I/POO-SmartHome/tests/test_dispositivos.py
@@ -1,26 +1,20 @@
-import unittest
+import pytest
 from dispositivos import Dispositivo
-class TestDispositivo(unittest.TestCase):
 
-    def setUp(self):
-        
-        self.dispositivo = Dispositivo(
-            id_dispositivo=1,
-            nombre_dispositivo="Sensor de temperatura",
-            ubicacion="Sala",
-            estado_dispositivo="activo",
-            id_tipo=2,
-            id_usuario=5
-        )
-        
-    def test_atributos_dispositivo(self):
-        self.assertEqual(self.dispositivo.id_dispositivo, 1)
-        self.assertEqual(self.dispositivo.nombre_dispositivo, "Sensor de temperatura")
-        self.assertEqual(self.dispositivo.ubicacion, "Sala")
-        self.assertEqual(self.dispositivo.estado_dispositivo, "activo")
-        self.assertEqual(self.dispositivo.id_tipo, 2)
-        self.assertEqual(self.dispositivo.id_usuario, 5)
-
-if __name__ == '__main__':
-    print("Ejecutando pruebas de la clase Dispositivo...\n")
-    unittest.main()
+def test_constructor_dispositivo():
+    dispositivo = Dispositivo(
+        id_dispositivo=1,
+        nombre_dispositivo="Sensor de temperatura",
+        ubicacion="Sala",
+        estado_dispositivo="activo",
+        id_tipo=2,
+        id_usuario=5
+    )
+    
+    assert dispositivo.id_dispositivo == 1
+    assert dispositivo.nombre_dispositivo == "Sensor de temperatura"
+    assert dispositivo.ubicacion == "Sala"
+    assert dispositivo.estado_dispositivo == "activo"
+    assert dispositivo.id_tipo == 2
+    assert dispositivo.id_usuario == 5
+    
