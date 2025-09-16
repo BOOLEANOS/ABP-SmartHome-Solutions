5dd7c0eca12103d5bafaaef04fa42335749bfaf9
Nanu <nancy.morales.387.06@gmail.com>
2025-09-15 22:27:20 -0300

    remove unnecessary files - pytest

--- PROGRAMACIÓN I/POO-SmartHome/tests/test_dispositivos.py
+++ PROGRAMACIÓN I/POO-SmartHome/tests/test_dispositivos.py
@@ -18,3 +18,24 @@ def test_constructor_dispositivo():
     assert dispositivo.id_tipo == 2
     assert dispositivo.id_usuario == 5
     
+def test_setters_dispositivo():
+    dispositivo = Dispositivo(
+        id_dispositivo=1,
+        nombre_dispositivo="Sensor de temperatura",
+        ubicacion="Sala",
+        estado_dispositivo="activo",
+        id_tipo=2,
+        id_usuario=5
+    )
+    
+    dispositivo.nombre_dispositivo = "Sensor de humedad"
+    dispositivo.ubicacion = "Cocina"
+    dispositivo.estado_dispositivo = "inactivo"
+    dispositivo.id_tipo = 3
+    dispositivo.id_usuario = 6
+    
+    assert dispositivo.nombre_dispositivo == "Sensor de humedad"
+    assert dispositivo.ubicacion == "Cocina"
+    assert dispositivo.estado_dispositivo == "inactivo"
+    assert dispositivo.id_tipo == 3
+    assert dispositivo.id_usuario == 6
