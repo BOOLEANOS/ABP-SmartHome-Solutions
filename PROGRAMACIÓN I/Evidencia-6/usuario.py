d0afacb42b3d6c9e0edec52ab48fb3c4d88aa834
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-10-10 20:14:58 -0300

    usuario.py

--- PROGRAMACIÓN I/Evidencia-6/usuario.py
+++ PROGRAMACIÓN I/Evidencia-6/usuario.py
@@ -1,1 +1,7 @@
-
+class Usuario:
+    def __init__(self, id, nombre, correo, contrasena, rol):
+        self.id = id
+        self.nombre = nombre
+        self.correo = correo
+        self.contrasena = contraseña
+        self.rol = rol
