216293ee0444ae4f643769ffbc63c128835e9841
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-10-10 20:31:55 -0300

    Update usuario.py

--- PROGRAMACIÓN I/Evidencia-6/usuario.py
+++ PROGRAMACIÓN I/Evidencia-6/usuario.py
@@ -3,5 +3,10 @@ class Usuario:
         self.id = id
         self.nombre = nombre
         self.correo = correo
-        self.contrasena = contraseña
+        self.contrasena = contrasena
         self.rol = rol
+
+    def _validar_correo(self):
+        import re
+        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        return re.match(pattern, self.correo) is not None
