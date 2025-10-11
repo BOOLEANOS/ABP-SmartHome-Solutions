8bd4ea4cb79bb0acdf5c7380621346a1152a4afd
Tobitototo24 <tobiasruffino8@hotmail.com>
2025-10-11 15:13:45 -0300

    Update db_conn.py

--- PROGRAMACIÓN I/Evidencia-6/conn/db_conn.py
+++ PROGRAMACIÓN I/Evidencia-6/conn/db_conn.py
@@ -1,1 +1,20 @@
+import mysql.connector
+from mysql.connector import Error
 
+def crear_conexion():
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",      
+            user="root",           
+            password="",           
+            database="smarthome"   
+        )
+        if conexion.is_connected():
+            return conexion
+    except Error as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+
+def cerrar_conexion(conexion):
+    if conexion and conexion.is_connected():
+        conexion.close()
